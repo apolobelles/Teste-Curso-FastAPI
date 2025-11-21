a174013e28cb6891c363d024ee689dd4835cc089
apolo <apk2mildois@gmail.com>
2025-11-21 16:18:48 -0300

    adicionei tabelas

--- PycharmProjects/FastAPI - WAPIX/auth_routes.py
+++ PycharmProjects/FastAPI - WAPIX/auth_routes.py
@@ -3,8 +3,8 @@ from fastapi import APIRouter
 auth_router = APIRouter(prefix='/auth', tags=['autenticação'])
 
 @auth_router.get('/')
-    async def autenticar():
-        '''
-     Essa é a rota de autenticação padrão do nosso sistema
-        '''
-        return{'mensagem': 'parabens voce acessou as autenticações', 'autenticado': False}
+async def autenticar():
+    '''
+Essa é a rota de autenticação padrão do nosso sistema
+    '''
+    return{'mensagem': 'parabens voce acessou as autenticações', 'autenticado': False}
--- PycharmProjects/FastAPI - WAPIX/models.py
+++ PycharmProjects/FastAPI - WAPIX/models.py
@@ -1,40 +1,64 @@
 from sqlalchemy import  create_engine, Column, String, Integer, Boolean, Float, ForeignKey
 from sqlalchemy.orm import declarative_base
+from sqlalchemy_utils.types import ChoiceType
 
 db = create_engine ('sqlite:///banco.db')
 
 Base = declarative_base()
 
- class Usuario(Base):
-        _tablename_ = 'usuarios'
+#usuarios
+class Usuario(Base):
+     _tablename_ = 'usuarios'
 
-        id = Column ('id', Integer, primary_key=True, autoincrement=True)
-        nome = Column ('nome', String)
-        email = Column ('email', String, nullable=False)
-        senha = Column ('senha', String)
-        ativo = Column ('ativo', Boolean)
-        adm = Column ('adm', Boolean, default=False)
+     id = Column ('id', Integer, primary_key=True, autoincrement=True)
+     nome = Column ('nome', String)
+     email = Column ('email', String, nullable=False)
+     senha = Column ('senha', String)
+     ativo = Column ('ativo', Boolean)
+     adm = Column ('adm', Boolean, default=False)
 
-        def _Init_(self, nome, email, senha, ativo=True, adm=False):
-            self.usuario = nome
+     def _Init_ (self, nome, email, senha, ativo=True, adm=False):
+            self.nome = nome
             self.email = email
             self.senha = senha
             self.ativo = ativo
             self.adm = adm
 
 
- class Pedido(Base):
+class Pedido(Base):
      _tablename_ = 'pedidos'
 
-     id = Column ('id', Integer, primary_key=True, autoincrement=True)
-     status = Column ('satus', String)
-     usuario = Column ('usuário', ForeignKey('usuarios.id'))
-     preco = Column ('preço', Float)
-     #itens = ()
+     STATUS_PEDIDOS = (
+        ('PENDENTE', 'PENDENTE'),
+        ('CANCELADO', 'CANCELADO'),
+        ('FINALIZADO', 'FINALIZADO')
+     )
 
-     def _init_(self, status='PENDENTE', usuario, preco=0, itens):
+     id = Column ('id', Integer, primary_key=True, autoincrement=True)
+     status = Column ('status', ChoiceType(choices=STATUS_PEDIDOS))
+     usuario = Column ('usuario', ForeignKey('usuarios.id'))
+     preco = Column ('preço', Float)
+
+     def _init_ (self, usuario, status='PENDENTE', preco=0):
          self.status = status
          self.usuario = usuario
          self.preco = preco
-         self.itens = itens
 
+
+class ItensPedidos(Base):
+      _tablename_ = 'Pizza'
+
+      id = Column ('id', Integer, primary_key=True, autoincrement=True)
+      quantidade = Column ('quantidade', Integer)
+      sabor = Column ('sabor', String)
+      tamanho = Column ('tamanho', String)
+      preco_unitario = Column ('preco_unitario', Float)
+      pedido = Column ('pedido', ForeignKey('pedidos.id'))
+
+    #São valores que não são gerados automaticamente (precisa de alguem pra digitar)
+      def _init_ (self, quantidade, sabor, tamanho, preco_unitario, pedido):
+             self.quantidade = quantidade
+             self.sabor = sabor
+             self.tamanho = tamanho
+             self.preco_unitario = preco_unitario
+             self.pedido = pedido
